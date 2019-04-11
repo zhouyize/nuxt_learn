04acb2e2b92d7fca7934a358d33e1e0928b0c9fb
zhouyize <853721595@qq.com>
2019-04-11 18:21:16 +0800

    commit

---
+++ layouts/search.vue
@@ -0,0 +1,53 @@
+<template>
+    <!--只能存在一个根容器-->
+    <div class="layout-search">
+      <h1>search layout header</h1>
+      <nuxt />
+      <footer>search layout footer</footer>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    components: {},
+    // props:[],
+    props: {
+      // nick:{
+      //  type:String,
+      //  default:'国民老公'
+      // },
+
+      // 对象或数组默认值必须从一个工厂函数获取
+      // girlFriends:{
+      //  type:Array,
+      //  default:function(){
+      //    return ['g1','g2']
+      // }
+      // }
+    },
+    // 所有的初始化状态全部放入data中
+    data() {
+      return {}
+    },
+    methods: {},
+    // 计算属性
+    computed: {},
+
+    // 实时监听数据变化，可以在这里做相关判断
+    watch: {}
+
+  }
+</script>
+
+<!--样式-->
+<!--
+Add "scoped" attribute to limit CSS to this component only
+添加scoped属性限制样式只在当前组件中生效
+-->
+<style scoped>
+
+  .layout-search{
+    color:red;
+  }
+</style>
---
+++ pages/search.vue
@@ -0,0 +1,65 @@
+<template>
+    <!--只能存在一个根容器-->
+    <div class="">
+      Page is search
+      <ul>
+        <li v-for="(item,index) in list" :key="index">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    layout: 'search',
+    name: '',
+    components: {},
+    // props:[],
+    props: {
+      // nick:{
+      //  type:String,
+      //  default:'国民老公'
+      // },
+
+      // 对象或数组默认值必须从一个工厂函数获取
+      // girlFriends:{
+      //  type:Array,
+      //  default:function(){
+      //    return ['g1','g2']
+      // }
+      // }
+    },
+    // 所有的初始化状态全部放入data中
+    data() {
+      return {
+        list: []
+      }
+    },
+
+    async mounted() {
+      let self = this
+      let {status, data: {list}} = await axios.get('/city/list')
+      if (status === 200) {
+        self.list = list
+      }
+    },
+    methods: {},
+    // 计算属性
+    computed: {},
+
+    // 实时监听数据变化，可以在这里做相关判断
+    watch: {}
+
+  }
+</script>
+
+<!--样式-->
+<!--
+Add "scoped" attribute to limit CSS to this component only
+添加scoped属性限制样式只在当前组件中生效
+-->
+<style scoped>
+
+</style>
